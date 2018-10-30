a11e3f898eb42c5582429f1394ac7db8129be18c
Chong Thao <tsoogthoj@hotmail.com>
2018-10-29 19:41:05 -0700

    added function to add staff's time

--- js/display.js
+++ js/display.js
@@ -16,9 +16,6 @@ $('#nav-staff-tab').click(function() {
     }, 500)
 })
 
-// date picker
-
-
 // limit pin input to 4
 var max_chars = 4;
 
@@ -46,128 +43,9 @@ $('#toggleAddStaffForm').click(function() {
 })
 
 
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDixWv9uLD8cMkTSjZ6CWxK1PIhgli_VyY",
-    authDomain: "time-clock-38dfc.firebaseapp.com",
-    databaseURL: "https://time-clock-38dfc.firebaseio.com",
-    projectId: "time-clock-38dfc",
-    storageBucket: "time-clock-38dfc.appspot.com",
-    messagingSenderId: "267709784938"
-};
-
-firebase.initializeApp(config);
-
-const db = firebase.firestore();
-
-db.settings({
-    timestampsInSnapshots: true
-});
-
-// add value of inputs to db
-$('#addNewStaffBtn').click( function(event) {
-    event.preventDefault();
-    
-    if ($('#firstName').val() === '' || $('#lastName').val() === ''|| $('#newPin').val() === '') {
-
-    } else {
-        
-        let firstName = $('#firstName').val();
-        let lastName = $('#lastName').val();
-        let pinNumber = $('#newPin').val();
-        
-
-        
-        db.collection("staff").add({
-            name: firstName + ' ' + lastName,
-            pin: pinNumber
-        })
-
-        // reset form
-        $('#firstName').val('');
-        $('#lastName').val('');
-        $('#newPin').val('');
-    }
-})
 
 
-// create element and render staff to staff
-function renderStaff(doc) {
-    tr = document.createElement('tr');
-    tr.setAttribute('data-id', doc.id);
-
-    let first = doc.data().name.split(' ')[0];
-    let last = doc.data().name.split(' ')[1];
-    tr.innerHTML = "<td>" + doc.data().name.split(' ')[0] + "</td><td>" + doc.data().name.split(' ')[1] + "</td><td>" + doc.data().pin + "</td>"
-    $('#displayStaffInfo').append(tr);
-}
-
-// create element and render staff to time sheet
-function renderStaffTime(doc) {
-    let data = doc.data();
-
-    let timeCardBodyRef = $('#timeCardBody');
-
-    // create table row with unquie ID 
-    let tr = document.createElement('tr');
-    tr.setAttribute('data-id', doc.id);
-
-    // get name and append to th
-    let firstTh = document.createElement('th');
-    firstTh.innerHTML = doc.data().name;
-    tr.append(firstTh);
-
-    // get clock in time and append to table
-    let secondTh = document.createElement('th');
-    let table = document.createElement('table');
-    let firstTr = document.createElement('tr');
-    let secondTr = document.createElement('tr');
-
-        // format time
-        let timeIn = data['10-17-2018'][0]
-        timeIn = moment(timeIn).format('hh:mm:ss a')
-        let timeOut = data['10-17-2018'][0]
-        timeOut = moment(timeOut).format('hh:mm:ss a');
-        let timeTotal = data['10-17-2018'][1] - data['10-17-2018'][0]
-        timeTotal = moment(timeTotal).format('mm:ss')
-
-        firstTr.innerHTML = '<th>In</th><th>Out</th><th>Total</th>';
-        secondTr.innerHTML = '<td>' + timeIn + '</td><td>' + timeOut + '</td><td>' + timeTotal + ' min</td>';
-
-        table.append(firstTr)
-        table.append(secondTr)
-
-        secondTh.append(table)
-        tr.append(secondTh)
-
-    // append everything to table
-    timeCardBodyRef.append(tr);
-}
-
-
-// get data from db
-db.collection('staff').orderBy('name').onSnapshot(snapshot => {
-    let changes = snapshot.docChanges();
-    changes.forEach(change => {
-        // get first name
-        if(change.type == 'added'){
-            renderStaff(change.doc);
-        }
-        renderStaffTime(change.doc)
-    });
-});
-
-
-db.collection("staff").get().then(function(querySnapshot) {
-    let staffId = []
-    querySnapshot.forEach(function(doc) {
-        doc.data()
-        staffId.push(doc.data().pin)
-    });
-});
-
-
-// Calendar
+/////////////// Calendar //////////////////
 const currentTime = moment();
 const startOfWeekMon = parseInt(currentTime.startOf('week').format('DD')) + 1;
 const endOfWeekFri = parseInt(moment().endOf('week').format('DD')) - 1;
@@ -215,7 +93,7 @@ function displayWeekdays() {
     $('.wed').children('span').text(displayText[2])
     $('.thu').children('span').text(displayText[3])
     $('.fri').children('span').text(displayText[4])
-
+    
 }
 
 
@@ -234,10 +112,10 @@ let increaseDays = function() {
             numberOfDays(weekArray[i]['year'], weekArray[i]['month'])
             weekArray[i]['month'] = parseInt(weekArray[i]['month']) + 1
             // going to next year
-                if (parseInt(weekArray[i]['month']) === 13) {
-                    weekArray[i]['month'] = 1
-                    weekArray[i]['year'] = parseInt(weekArray[i]['year']) + 1
-                }
+            if (parseInt(weekArray[i]['month']) === 13) {
+                weekArray[i]['month'] = 1
+                weekArray[i]['year'] = parseInt(weekArray[i]['year']) + 1
+            }
             weekArray[i]['day'] = startingDay
             startingDay++
         }
@@ -263,7 +141,7 @@ function displayDates() {
             firstMonth = monthAbr[i]
         }
     }
-
+    
     let monday = weekArray[0]['day']
     let firstYear = ", " + weekArray[0]['year']
     let secondMonth = weekArray[4]['month']
@@ -273,7 +151,7 @@ function displayDates() {
             secondMonth = monthAbr[i]
         }
     }
-
+    
     let friday = weekArray[4]['day']
     let secondYear = ", " + weekArray[4]['year']
     // check same year
@@ -297,7 +175,7 @@ function backWeek() {
         weekArray[0]['day'] = parseInt(daysInMonth) + parseInt(weekArray[0]['day'])
         weekArray[0]['month'] = parseInt(weekArray[0]['month']) - 1
     }
-
+    
     if (parseInt(weekArray[0]['day']) === 0) {
         weekArray[0]['month'] = parseInt(weekArray[0]['month']) - 1
         if (parseInt(weekArray[0]['month']) === 0) {
@@ -315,7 +193,7 @@ function backWeek() {
 function forwardWeek() {
     weekArray[0]['day'] = weekArray[0]['day'] + 7
     numberOfDays(weekArray[0]['year'], weekArray[0]['month'])
-
+    
     if (parseInt(weekArray[0]['day']) > daysInMonth) {
         weekArray[0]['day'] = weekArray[0]['day'] - daysInMonth
         weekArray[0]['month'] = parseInt(weekArray[0]['month']) + 1
@@ -337,16 +215,229 @@ let getCurrent = function() {
 }   
 getCurrent()
 
-// button click
-$('#leftArrowBtn').on('click', function() {
-    backWeek()
-})
-$('#rightArrowBtn').on('click', function() {
-    forwardWeek()
-})
-$('#todayBtn').on('click', function() {
-    getCurrent();
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDixWv9uLD8cMkTSjZ6CWxK1PIhgli_VyY",
+    authDomain: "time-clock-38dfc.firebaseapp.com",
+    databaseURL: "https://time-clock-38dfc.firebaseio.com",
+    projectId: "time-clock-38dfc",
+    storageBucket: "time-clock-38dfc.appspot.com",
+    messagingSenderId: "267709784938"
+};
+
+firebase.initializeApp(config);
+
+
+const db = firebase.firestore();
+
+db.settings({
+    timestampsInSnapshots: true
+});
+
+//////////////// Staff information display /////////////////
+// push pins into array for pin validation
+let pins = []
+let pinInUse = false
+
+db.collection('staff').orderBy('name').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges()
+    changes.forEach(change => {
+        if (change.type == 'added') {
+            pins.push(change.doc.data().pin)
+        }
+    }) 
 })
 
-// moment().date() = #(1-5)
-// 1 = mom / 2 = tue / 3 = wed / 4 = thu / 5 = fri 
+// add value of inputs to db
+$('#addNewStaffBtn').click( function(event) {
+    event.preventDefault();
+    if ($('#firstName').val() === '' || $('#lastName').val() === ''|| $('#newPin').val() === '') {
+        alert('Enter each field correctly.')
+    } else {
+        let firstName = $('#firstName').val();
+        let lastName = $('#lastName').val();
+        let pinNumber = $('#newPin').val();
+        for (let i = 0; i < pins.length; i++) {
+            if (pinNumber === pins[i]) {
+                pinInUse = true
+            }
+        }
+        if (pinInUse) {
+            alert('Duplication of Pin. Enter a new pin.')
+            pinInUse = false
+        } else {
+            db.collection('staff').add({
+                name: firstName + ' ' + lastName,
+                pin: pinNumber
+            })
+            
+            // reset form
+            $('#firstName').val('')
+            $('#lastName').val('')
+            $('#newPin').val('')
+        }
+        
+    }
+})
+
+// function to display staff's information
+function renderStaff(doc) {
+    let data = doc.data()
+    let tbodyRef = $('#displayStaffInfo')
+    let tbodyStaffDisplay = document.createElement('tbody')
+    let tr = document.createElement('tr')
+    let tdFirstName = document.createElement('td')
+    let tdLastName = document.createElement('td')
+    let tdPin = document.createElement('td')
+    
+    // set id to tr
+    tr.setAttribute('data-id', doc.id)
+    tbodyRef.append(tr)
+    
+    // set names and pin to td
+    tdFirstName.innerHTML = doc.data().name.split(' ')[0]
+    tdLastName.innerHTML = doc.data().name.split(' ')[1]
+    tdPin.innerHTML = doc.data().pin
+    tr.append(tdFirstName)
+    tr.append(tdLastName)
+    tr.append(tdPin)
+}
+
+//////////////// Time Clock Sheet ////////////////
+// get times in and out
+function getTime(doc) {
+    let data = doc.data()
+    // console.log(data.Oct292018)
+    $('.timeIn').change(function() {
+        console.log('Moved')
+    })
+}
+// create element and render staff to time sheet
+function renderStaffTime(doc) {
+    let data = doc.data();
+
+    
+    let timeCardBodyRef = $('.tableContent')
+    // timeCardBodyRef.empty()
+    // create table row with unquie ID 
+    let tr = document.createElement('tr')
+    timeCardBodyRef.append(tr)
+    
+    /////// staff name ///////////
+    // get name and append to th
+    let staffName = document.createElement('td')
+    staffName.setAttribute('class', 'staffName')
+    staffName.setAttribute('data-id', doc.id)
+    staffName.innerHTML = data.name;
+    tr.append(staffName)
+    
+    /////// display week /////////// 
+    let monthArray = []
+    let nameArray = []
+    for (let i = 0; i < 5; i++) {
+        monthArray.push(weekArray[i]['month'])
+    }
+    numberToMonth(monthArray[0], nameArray)
+    numberToMonth(monthArray[1], nameArray)
+    numberToMonth(monthArray[2], nameArray)
+    numberToMonth(monthArray[3], nameArray)
+    numberToMonth(monthArray[4], nameArray)
+
+    for (let i = 0; i < 5; i++) {
+        let tdDay = document.createElement('td')
+        tdDay.setAttribute('class', 'dayOfWeek')
+        // table head for in and out text
+        let tableDay = document.createElement('table')
+        let theadDay = document.createElement('thead')
+        let trInOutHeading = document.createElement('tr')
+        trInOutHeading.setAttribute('class', 'staffTimeHead')
+        trInOutHeading.innerHTML = '<th>In</th><th>Out</th><th>Total</th>'
+        theadDay.append(trInOutHeading)
+        tableDay.append(theadDay)
+        tdDay.append(tableDay)
+        tr.append(tdDay)
+        let timeTbody = document.createElement('tbody')
+        let timeTr = document.createElement('tr')
+        let inTd = document.createElement('td')
+        inTd.setAttribute('class', 'timeIn')
+
+        let outTd = document.createElement('td')
+        outTd.setAttribute('class', 'timeOut')
+
+        let times = data[nameArray[i] + weekArray[i]['day'] + weekArray[i]['year']]
+        let loggedTime = []
+        for (const key in times) {
+            if (times.hasOwnProperty(key)) {
+                loggedTime.push(times[key])
+            }
+        }
+        inTd.append( moment(parseInt(loggedTime[0])).format('hh:mm:ss a') )
+        outTd.append( moment(parseInt(loggedTime[1])).format('hh:mm:ss a') )
+        totalTime = parseInt(loggedTime[1]) - parseInt(loggedTime[0])
+        let totalTd = document.createElement('td')
+        totalTd.setAttribute('class', 'timeTotal')
+        totalTd.append( moment(parseInt(totalTime)).format('mm:ss') )
+        timeTr.append(inTd)
+        timeTr.append(outTd)
+        timeTr.append(totalTd)
+        timeTbody.append(timeTr)
+        tableDay.append(timeTbody)
+        tdDay.append(tableDay)
+        tr.append(tdDay)
+    }
+}
+
+// render staff name and staff in and out times
+db.collection('staff').orderBy('name').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges()
+    changes.forEach(change => {
+        if (change.type == 'added') {
+            // console.log(change.doc.id)
+            renderStaff(change.doc)
+            renderStaffTime(change.doc)
+            getTime(change.doc)
+        }
+    }) 
+})
+
+
+function displayStaffTime() {
+    db.collection('staff').onSnapshot(snapshot => {
+        let changes = snapshot.docChanges()
+        changes.forEach(change => {
+            if (change.type == 'added') {
+                // table for staff times
+                renderStaffTime(change.doc)
+            }
+        }) 
+    })
+}
+function numberToMonth(number, nameArray) {
+    for (let i = 0; i < monthAbr.length; i++) {
+        if (parseInt(number) === i + 1) {
+            number = monthAbr[i]
+            nameArray.push(monthAbr[i])
+        }
+    }
+}
+
+// button click
+$('#leftArrowBtn').on('click', function() {
+    $('.tableContent').empty()
+    backWeek()
+    getDates()
+    displayStaffTime()
+})
+$('#rightArrowBtn').on('click', function() {
+    $('.tableContent').empty()
+    forwardWeek()
+    getDates()
+    displayStaffTime()
+})
+$('#todayBtn').on('click', function() {
+    $('.tableContent').empty()
+    getCurrent()
+    getDates()
+    displayStaffTime()
+})
--- js/index.js
+++ js/index.js
@@ -26,88 +26,83 @@ let wrongSound = new Audio('audio/wrong.mp3')
 wrongSound.loop = false
 
 
-db.collection("staff").get().then(function(querySnapshot) {
-	let staffPin = []
-	let staffNames = []
-	let idTag = []
-    querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
-        // console.log(doc.id, " => ", doc.data());
-		staffPin.push(doc.data().pin)
-		staffNames.push(doc.data().name)
-		idTag.push(doc.id)
-	});
-	// console.log(idTag)
-	// console.log(staffNames);
+let staffPin = []
+let staffNames = []
+let idTag = []
+
+db.collection('staff').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges()
+    changes.forEach(change => {
+        if (change.type == 'added') {
+			staffPin.push(change.doc.data().pin)
+			staffNames.push(change.doc.data().name)
+			idTag.push(change.doc.id)
+        }
+    }) 
+})
+
 
 // Click
-	$('.number').click(function() {
-		selected = $(this)
-		input += $(this).text()
-		// console.log(input)
-		// console.log(input.length)
-		$(this).css("border","1px solid white")
-		$(this).css('background', 'gray')
-		setTimeout(function() {
-			selected.css("border","1px solid gray")
-			selected.css('background', '#212121')
-		},200)
-		if (input.length === 1) {
-			$('.dot1').css('background','white')
-		}
-		if (input.length === 2) {
-			$('.dot2').css('background','white')
-		}
-		if (input.length === 3) {
-			$('.dot3').css('background','white')
-		}
-		if (input.length === 4) {
-			$('.dot4').css('background','white')
-			let found = 0;
-			let currentId = '';
-			for (let i = 0; i < staffPin.length; i++) {
-				if (input === staffPin[i]) {
-					currentId = idTag[i]
-					// console.log('found')
-					found = 1;
-					break
-				}
+$('.number').click(function() {
+	selected = $(this)
+	input += $(this).text()
+	// console.log(input)
+	// console.log(input.length)
+	$(this).css("border","1px solid white")
+	$(this).css('background', 'gray')
+	setTimeout(function() {
+		selected.css("border","1px solid gray")
+		selected.css('background', '#212121')
+	},200)
+	if (input.length === 1) {
+		$('.dot1').css('background','white')
+	}
+	if (input.length === 2) {
+		$('.dot2').css('background','white')
+	}
+	if (input.length === 3) {
+		$('.dot3').css('background','white')
+	}
+	if (input.length === 4) {
+		$('.dot4').css('background','white')
+		let found = 0;
+		let currentId = '';
+		for (let i = 0; i < staffPin.length; i++) {
+			if (input === staffPin[i]) {
+				currentId = idTag[i]
+				// console.log('found')
+				found = 1;
+				break
 			}
-			// console.log(currentId)
-			if (found) {
-				todayDate = moment().format('MM-DD-YYYY');
-				todayMonth = moment().format('MM')
-				// console.log(todayMonth)
-				time = moment().valueOf();
-				// console.log(time);
+		}
+		// console.log(currentId)
+		if (found) {
+			todayDate = moment().format('MMMDDYYYY');
+			todayMonth = moment().format('MM')
+			// console.log(todayMonth)
+			time = moment().valueOf();
+			// console.log(time);
 
-				db.collection("staff").doc(currentId).update({
-					[todayDate]: firebase.firestore.FieldValue.arrayUnion(time)
-				})
-				db.collection("month").doc(todayMonth).update({
-					day: firebase.firestore.FieldValue.arrayUnion('10-17-2018')
-				})
+			db.collection("staff").doc(currentId).update({
+				[todayDate]: firebase.firestore.FieldValue.arrayUnion(time)
+			})
 
-				correctSound.play();
-				$('body').addClass('correct');
-				setTimeout(function() {
-					$('body').removeClass('correct')
-				},500)
-			} else {
-				wrongSound.play()
-				$('body').addClass('wrong')
-				setTimeout(function() {
-					$('body').removeClass('wrong')
-				},500)
-			}
-			
+			correctSound.play();
+			$('body').addClass('correct');
 			setTimeout(function() {
-				input = '';
-				$('.dot').css('background','rgba(255, 255, 255, 0.5)')
+				$('body').removeClass('correct')
+			},500)
+		} else {
+			wrongSound.play()
+			$('body').addClass('wrong')
+			setTimeout(function() {
+				$('body').removeClass('wrong')
 			},500)
 		}
-	})
-
-// db
-});
-
+		
+		setTimeout(function() {
+			input = '';
+			$('.dot').css('background','rgba(255, 255, 255, 0.5)')
+		},500)
+	}
+})
